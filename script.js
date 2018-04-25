c4dac4bc5a8948d455e53ca128707726ad2b26ca
Kamil Sztefko <sztefkokamil@gmail.com>
2018-04-25 13:21:57 +0200

    menu add

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
+
+$("nav").hide();
+
 $(document).ready(function() {
 
+  // window size ----------------------
+  const $window = $(window);
+  let W = $window.width();
+  let H = $window.height();
 
-  // ----- MAP -----------------------------
+  $window.resize(()=>{
+    W = $window.width();
+    H = $window.height();
+  }); // window size --------------------
+
+
+  // open menu ----------------------------
+  const $menuBtn = $(".menuBtn");
+  const $nav = $("nav");
+  const $upperBar = $(".upperBar");
+
+  $menuBtn.click(()=>{
+
+    if($menuBtn.hasClass("active")){
+      $menuBtn.removeClass("active");
+      $nav.slideUp(300);
+      setTimeout(() => {
+        $upperBar.removeClass("active");
+      }, 300);
+    }
+    else{
+      $upperBar.addClass("active");
+      $menuBtn.addClass("active");
+      $nav.slideDown(300);
+    }
+  }); // open menu ------------------------
+
+
+
+  map();
+  
+}); // ready -----------------
+
+
+
+
+function map(){ // --------------------------------
+      
   $('.map').goMap({
-      latitude : 52.2230863,
-      longitude : 20.9850418,
-      zoom : 16,
-      scaleControl : true,
-      maptype : 'ROADMAP'
+    latitude : 52.2230863,
+    longitude : 20.9850418,
+    zoom : 16,
+    scaleControl : true,
+    maptype : 'ROADMAP'
   }); // goMap
-  
+
   $.goMap.createMarker({  
-       latitude : 52.223214,
-       longitude :  20.984970,
-       title : 'Tu jesteśmy',
-       html : {
-       popup : true   
-       } // createMarker
+     latitude : 52.223214,
+     longitude :  20.984970,
+     title : 'Tu jesteśmy',
+     html : {
+     popup : true   
+     } // createMarker
   }); 
-  // ----- MAP -------------------------------
-  
-  }); // ready -----------------
+
+} // map function --------------------------
